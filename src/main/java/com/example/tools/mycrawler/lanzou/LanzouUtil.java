43a7966be8973cbef3924ae59d0f4e632c89989d
yi_jing <yi_jing@hunliji.com>
2022-06-21 10:00:05 +0800

    城通 download 蓝奏

--- src/main/java/com/example/tools/mycrawler/lanzou/LanzouUtil.java
+++ src/main/java/com/example/tools/mycrawler/lanzou/LanzouUtil.java
@@ -42,8 +42,7 @@ public class LanzouUtil {
 
     public static void main(String[] args) throws IOException {
 
-        FileUtils.writeLines(new File(dir,"files.txt"),
-                FileUtils.listFiles(new File(dir),null,false).stream().map(File::getName).collect(Collectors.toList()));
+        //FileUtils.writeLines(new File(dir,"files.txt"), FileUtils.listFiles(new File(dir),null,false).stream().map(File::getName).collect(Collectors.toList()));
 
 
         /*String a = "https://develope.lanzoug.com/file/?UDYCPAEwBTRTWgA4AzZcMFZpBj4DugKyVupRs1y4BpFQv1O4D8hS4wX3UaBX5VfuB9hXt1+4C5QH5QCdULEH4VDKAtEBuQXUU7EAsgPtXPxW6QaTA/MC51b1Ue1cLgZ9UDlTdA90UmAFOlFrV2ZXCQc0VzZfPQs+BzMAN1A7BzVQaAJlAW8Fd1NjACUDalxuVjwGMANrAjBWYlFgXCYGd1AgUzkPYFI2BWFRNVcsV2YHa1d9XzELPgcvAGBQbwdjUGkCYwFuBTVTMQBuA2Ncb1Y/BjEDPwJmVjdRM1w3BjRQNVNgD2tSMQViUWNXZ1cwB2hXMF9iC2sHYQAoUHYHb1AgAnMBKQUiU2AAJAM+XDlWMQYyA2oCNFZkUWZcMwY0UHZTcA87UmsFNlFhVz5XZwdtV2JfMAs5By4AKFAqB2BQPAIiAWEFYFMzAGMDYlxrVj4GNQNuAjBWYVFxXHUGd1AnUzkPY1IwBWZRMlc3V2QHZVdmXzMLOgcmAHNQZQd2UG0CZAFuBWRTKwBnA2ZcalYiBjIDZAIuVmBRYlwy";
@@ -111,31 +110,24 @@ public class LanzouUtil {
             String a = listPage.select("body > script").toString();
             int i = a.indexOf("data : '");
             int mi = a.indexOf("data : {");
+            int fi = a.indexOf("function file");
             if(i > 0 ){
                 int j = a.indexOf("'", i + 8);
                 String u = a.substring(i + 8, j);
                 String[] aa = u.split("=|&");
                 return new String[]{aa[0],aa[1],aa[2],aa[3],aa[4],pwd};
-            }else if(mi > 0){
-                String b = a.substring(mi + 1,a.indexOf("}", mi));
-
-                /*
-
-                var ajaxdata = '?ctdf';
-		//var ispostdowns = '';
-		var websignkey = '1IZn';
-		var websign = '';
-		$.ajax({
-			type : 'post',
-			url : '/ajaxm.php',
-			data : { 'action':'downprocess','signs':ajaxdata,'sign':'A2VXaQo7ADEJAAE_bV2dUaAdrAzNXMAo_aUmVWYVM9UWlVc1d0C2sBZARkVDcKa1BmBm9TblI4ATEANA_c_c','ves':1,'websign':websign,'websignkey':websignkey },
-			//data : { 'action':'downprocess','sign':sign,'sign':sign,'ves':1},
-
-
-
-                 *
-                 */
-                return null;
+            }else if(mi > 0 && fi < 0){
+                String b = a.substring(mi + 8,a.indexOf("}", mi));
+                String[] aa = b.replace("'","").split(":|,");
+                String[] p = Arrays.stream(aa).map(String::trim).toArray(String[]::new);
+                String c = a.substring(a.indexOf("var ajaxdata"), a.indexOf("$.ajax"));
+                Pattern pa = Pattern.compile("\\=(.*?)\\;");//正则表达式，取=和|之间的字符串，不包括=和|
+                Matcher m = pa.matcher(c);
+                if(m.find()) p[3] = m.group(1).trim().replace("'","");
+                if(m.find()) m.group(1);
+                if(m.find()) p[11] = m.group(1).trim().replace("'","");
+                if(m.find()) p[9] = m.group(1);
+                return p;
             }else {
                 String b = a.substring(a.indexOf("$.ajax") + 8,a.indexOf("dataType"));
                 b = b.substring(b.indexOf("{") + 1,b.indexOf("}"));
@@ -189,10 +181,11 @@ public class LanzouUtil {
         return book;
     }
     private static String getUrlbyid(String id){
-        String u = "https://tianlangbooks.lanzouf.com/"+id;
+        String root = "https://tianlangbooks.lanzouf.com";
+        String u = root+"/"+id;
         return doRetry(3, () -> {
             Document listPage = Jsoup.connect(u).get();
-            return listPage.select("body > div > div > div > iframe").attr("src");
+            return root + listPage.select("body > div > div > div > iframe").attr("src");
         });
     }
 
